a4223a0e7e42148bf2cdcabd88faeb67adf6458c
meng980626 <42561397+meng980626@users.noreply.github.com>
2022-10-08 17:19:18 +0800

    Add files via upload

---
+++ torch/compile_pytorch.sh
@@ -0,0 +1,12 @@
+source /root/anaconda3/bin/activate  /root/anaconda3/envs/pytorch_5
+
+scp root@10.11.108.112:$SourceCodePath/pytorch/torch/lib/c10d/reducer.cpp $SourceCodePath/pytorch/torch/lib/c10d/
+scp root@10.11.108.112:$SourceCodePath/pytorch/torch/lib/c10d/reducer.hpp $SourceCodePath/pytorch/torch/lib/c10d/
+scp root@10.11.108.112:$SourceCodePath/pytorch/torch/lib/c10d/comm.hpp $SourceCodePath/pytorch/torch/lib/c10d/
+scp root@10.11.108.112:$SourceCodePath/pytorch/torch/csrc/distributed/c10d/init.cpp $SourceCodePath/pytorch/torch/csrc/distributed/c10d
+
+export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
+
+cd $SourceCodePath/pytorch
+
+python setup.py install
